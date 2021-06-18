0b284a652380b41ebe2252cb6e02b8649f00c516
zhangds <1282906135@qq.com>
2021-06-18 11:00:39 +0800

    21年6月13更新：线程池的加载

---
+++ src/com/zhangds/thread/pool/PoolCreate.java
@@ -0,0 +1,10 @@
+package com.zhangds.thread.pool;
+
+import java.util.concurrent.Executors;
+
+public class PoolCreate {
+
+    public static void main(String[] args) {
+        Executors.newCachedThreadPool();
+    }
+}
